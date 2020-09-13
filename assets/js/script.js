31ab6eaaf8c9ba485ad0835c351344cbbb4af0ab
terahje <terahjequinn@gmail.com>
2020-09-12 21:47:46 -0700

    add landing page

--- assets/js/script.js
+++
@@ -1,8 +0,0 @@
-// needed if using Materialize to initiate the select menu
-// this is currently not working:
-var instance = M.FormSelect.getInstance(elem);
-
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems, options);
-  });
